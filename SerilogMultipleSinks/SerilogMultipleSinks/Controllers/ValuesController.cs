e0c107bc56947fdd7892755fbed9e0afbf6b593c
chrisjoyce54 <chris@joycenet.us>
2019-06-12 07:29:22 -0500

    Add files via upload

---
+++ SerilogMultipleSinks/SerilogMultipleSinks/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace SerilogMultipleSinks.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		private ILogger logger;
+		ValuesController(ILogger logger)
+		{
+			this.logger = logger;
+		}
+		// GET api/values
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			logger.Debug("Get");
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			logger.Debug("Get Id");
+			return "value";
+		}
+
+		// POST api/values
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ SerilogMultipleSinks/SerilogMultipleSinks/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace SerilogMultipleSinks
+{
+	public class Program
+	{
+		public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+			.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+			.AddEnvironmentVariables()
+			.Build();
+
+		public static void Main(string[] args)
+		{
+			try
+			{
+				var file = File.CreateText(Path.Combine(Configuration.GetSection("DataFolder").Value, "self2.log"));
+				Serilog.Debugging.SelfLog.Enable(TextWriter.Synchronized(file));
+				Log.Information("PSCU Listener service starting ...");
+				Log.Verbose("Configuring Logger");
+
+				Log.Logger = new LoggerConfiguration()
+					.ReadFrom.Configuration(Configuration)
+					.CreateLogger();
+
+				Log.Verbose("lOGGER cONFIGURED");
+
+				CreateWebHostBuilder(args)
+					.UseSerilog()
+					.Build().Run();
+
+				Log.Warning("Service Started");
+			}
+			catch (Exception e)
+			{
+
+			}
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>();
+	}
+}
